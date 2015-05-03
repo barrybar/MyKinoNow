533f6f6c066a49db672b8828c668e68bfdc69011
Barry Walsh <barryswalsh89@gmail.com>
2015-05-03 19:41:08 +0100

    Other Cinemas Option
    Added in option to return Other Cinemas near the user

--- js/whatToWatch.js
+++ js/whatToWatch.js
@@ -1,11 +1,12 @@
 
-        var geocoder, apikey, baseUrl, showTimeUrl1, showTimeUrl2, nearLocation, movies, moviesSearchUrl, query, cinemaName, movieResult;
+        var geocoder, apikey, baseUrl, showTimeUrl1, showTimeUrl2, nearLocation, movies, moviesSearchUrl, query, cinemaName, movieResult, otherTheatres, mainDataObject;
         showTimeUrl1 = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'http%3A%2F%2Fgoogle.com%2Fmovies%3Fnear%3D";
         showTimeUrl2 = "'%20&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
         namesArray = [];
         movies = [];
         moviesArray = [];
-        movieNamesArray = []
+        movieNamesArray = [];
+        otherTheatres = [];
         movieResult = {};
         // construct the url with our apikey
 
@@ -13,6 +14,7 @@
             geocoder = new google.maps.Geocoder();
             getLocation();
             $('#rated').hide();
+            $('#otherCinemas').hide();
             /*
              * Add event listeners to buttons
              */
@@ -27,6 +29,9 @@
             $('#button3').click(function() {
               outputToScreen(moviesArray[movieNamesArray.length-3]);
               changeClass('3');
+            });
+            $('#otherCinemas').click(function() {
+              outputOtherCinemas();
             });
 
         });
@@ -66,7 +71,7 @@
             nearLocation = nearLocation.replace(/'/g, '%2527');
             nearLocation = nearLocation.replace(/,/g, '%252C');
             /*
-             * Ajax call yo get the location of the user
+             * Ajax call to get the location of the user
              */
             $.ajax({
                 url: showTimeUrl1 + nearLocation + showTimeUrl2,
@@ -94,6 +99,30 @@
                 }
             });
         }
+        /* 
+         * get the movie Details
+         */
+        function getMovieDetails(obj) {
+          var movieO;
+           $.each(obj, function(index, movie) {
+                        movieO = new Object();
+                        movieO.name = movie.div[0].a.content;
+                        movieO.times  = [];
+                        /*
+                         * Check it's an array before looping through it, if it's an object, just push the one time through
+                         */
+                        if (movie.div[1].span.length){
+                          $.each(movie.div[1].span, function(index, time) {
+                          movieO.times.push(time.content);
+                          });
+                        } else {
+                          movieO.times.push(movie.div[1].span.content);
+                        }
+                        
+                        movieNamesArray.push(movieO);
+                    });
+        }
+        
 
 
         // Build up movie object
@@ -105,17 +134,26 @@
              */
             $.each(divs, function(index, div) {
                 if (div.id && div.id === "results") {
+                    /*
+                     * Set the main data object
+                     */
+                     mainDataObject = div;
                     movieResults = div.div.div.div;
                     cinemaName = movieResults[0].div[0].div.content;
                     movieObj = movieResults[0].div[1];
                     movieObjLeft = movieObj.div[0].div;
                     movieObjRight = movieObj.div[1].div;
-                    $.each(movieObjLeft, function(index, movie) {
-                        movieNamesArray.push(movie.div[0].a.content)
-                    });
-                    $.each(movieObjRight, function(index, movie) {
-                        movieNamesArray.push(movie.div[0].a.content)
-                    });
+                    /*
+                     * Loop through the left object
+                     */
+                     getMovieDetails(movieObjLeft);
+                    /*
+                     * Loop through the right object
+                     */
+                     getMovieDetails(movieObjRight);
+                    /*
+                     * Call function to get the IMDB details of the movie
+                     */ 
                     getRatings(movieNamesArray);
                 }
             });
@@ -128,7 +166,7 @@
                 today = new Date();
                 year = today.getFullYear();
                 $.ajax({
-                    url:  'http://www.omdbapi.com/?t='+ movie +'&y= ' + year +'&r=json&tomatoes=true',
+                    url:  'http://www.omdbapi.com/?t='+ movie.name +'&y= ' + year +'&r=json&tomatoes=true',
                     dataType: "jsonp",
                     success: searchCallback
                 });
@@ -162,7 +200,6 @@
         /*
          * callback for when we get back the results
          */ 
-        
         function searchCallback(data) {
                 moviesArray.push(data);
                 /*
@@ -172,21 +209,68 @@
                 
             }
 
+        /* 
+         * output the cinema Times
+         */    
+        function outputCinemaTime(p_m){
+          var times;
+          times = [];
+          $.each(movieNamesArray, function(index, movie) {
+              if (movie.name === p_m.Title) {
+                $('#left').append('<h2> When? </h2>');
+                $.each(movie.times, function(index, time){
+                  times.push(' ' + time);
+                });
+                $('#left').append('<h3>' + times + '</h3>');
+              }
+             });
+        } 
+        /*
+         * Output other cinemas
+         */
+
+         function outputOtherCinemas() {
+            divs = mainDataObject.div.div.div;
+            console.log(divs);
+            $.each(divs, function(index, div){
+                    //otherTheatres.push(div.div[0].div.content); 
+                     $('#otherCinemas').append('<h3>' + div.div[0].div.content + '</h3>');
+            });
+            console.log(otherTheatres);
+         }
+        /*
+         * What gets outputted to the screen
+         */
         function outputToScreen(p_movieResult) {
-        var m, image, time; 
+        var m, image;
         m = p_movieResult;
         image = m.Poster;
-        time = '20:30';
+            /*
+             * Clear any content there before
+             */
              $('#left').empty();
              $('#right').empty();
+             /*
+              * Add in the the title, cinema name, times and ratings
+              */
              $('#left').append('<h2> What? </h2>');
              $('#left').append('<h3>' + m.Title + '</h3>');
              $('#left').append('<h2> Where? </h2>');
              $('#left').append('<h3>' +  cinemaName + '</h3>');
+             outputCinemaTime(m);
              $('#left').append('<h2> Rating? </h2>');
              $('#left').append('<h3>' + m.imdbRating + '</h3>');
              $('#right').append('<img src = "' + image + '">');
+             /*
+              * Show buttons hidden at the start
+              */
              $('#rated').show();
+             $('#otherCinemas').show();
+             /*
+              * Change the class of the button
+              */
              changeClass('1');
+
+             
 
         }    
